d66a0b046146bbc7dae51f5237bb05fd11e8a89a
Islam Sulaiman <i.sulaiman@qoyod.com>
2024-03-05 21:00:27 +0200

    fix: check first if the user submitted the form before invoking the service

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,7 +1,6 @@
 class FlickrController < ApplicationController
   def index
-    sanity_check
-    @photos = FlickrService.new(params[:flickr_user_id]).get_photos_for_user
+    call_flickr_service if eligible
   rescue ArgumentError => e
     logger.error "Error: #{e.message}"
     flash.alert = e.message
@@ -9,8 +8,16 @@ class FlickrController < ApplicationController
 
   private
 
+  def call_flickr_service
+    sanity_check
+    @photos = FlickrService.new(params[:flickr_user_id]).get_photos_for_user
+  end
+
+  def eligible
+    true unless params[:form_submitted].blank?
+  end
+
   def sanity_check
-    # byebug
     raise ArgumentError, "Flickr user ID is missing or empty" if params[:flickr_user_id].blank?
   end
 end
