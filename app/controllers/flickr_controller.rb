4c55f36c04ef5ea2c742f006c6f93ea48b762cb6
Islam Sulaiman <i.sulaiman@qoyod.com>
2024-03-05 15:36:30 +0200

    feat: show error messages in the view

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,9 +1,19 @@
 class FlickrController < ApplicationController
   def index
-    begin #Necessary in case the user is not found
-      @photos = FlickrService.new(params[:flickr_user_id]).get_photos_for_user unless params[:flickr_user_id].nil?
-    rescue => e
-      flash.alert = "User Not Found... Sorry. Check and try again"
-    end
+    sanity_check
+    @photos = FlickrService.new(params[:flickr_user_id]).get_photos_for_user
+  rescue ArgumentError => e
+    logger.error "Error: #{e.message}"
+    flash.alert = e.message
+  rescue => e
+    logger.error "Error fetching photos: #{e.message}"
+    flash.alert = "Unexpected error occurred. Please try again."
+  end
+
+  private
+
+  def sanity_check
+    # byebug
+    raise ArgumentError, "Flickr user ID is missing or empty" if params[:flickr_user_id].blank?
   end
 end
