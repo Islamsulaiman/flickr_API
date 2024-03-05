26ee24981469c5ce211ddd9831530498ba2ba732
Islam Sulaiman <i.sulaiman@qoyod.com>
2024-03-05 10:33:13 +0200

    feat: create flickr service

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,4 +1,9 @@
 class FlickrController < ApplicationController
   def index 
+    if params[:flickr_user_id].present?
+      # Fetch photos from Flickr based on the provided user ID
+      # This is a placeholder logic; you'll need to replace it with actual Flickr API call
+        @photos = FlickrService.get_photos_for_user(params[:flickr_user_id])
+    end
   end
 end
---
+++ app/services/flickr_service.rb
@@ -0,0 +1,4 @@
+class FlickrService
+  def get_photos_for_user (user_id)
+  end
+end
